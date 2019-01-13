7cf860507bdacb4599d9575188d1a94644cda830
diogosantosmendes <diogosantosmendes@hotmail.com>
2019-01-13 10:49:52 +0000

    update

--- src/utils.js
+++ src/utils.js
@@ -5,38 +5,13 @@ var db = require('./models/index'),
 exports.validateToken = (token) => {
   return new Promise((resolve, reject) => {
 
-    let public_key = fs.readFileSync(__dirname + '/keys/cert.pem').toString();
-    if (public_key === undefined) reject("Server cannot validate the token");
-
     let options = {
       algorithms: ["RS256"]
     };
 
-    jwt.verify(token, public_key, options, (error, payload) => {
+    jwt.verify(token, process.env.PUBLIC_KEY, options, (error, payload) => {
       if (error) reject("Invalid token provided");
       else resolve();
-
-      // // verify if user
-      // if (payload.role === "User") db.User.findById(payload.id).then(
-      //   user => {
-      //     // if is a user, set its own exchange
-      //     if (user) user.getVitaboxes({ where: { active: true } }).then(
-      //       vitaboxes => {
-      //         // push exchanges to vitaboxes
-      //         let rooms = vitaboxes.map(x => x.id);
-      //         if (user.doctor) user.getPatients().then(
-      //           patients => {
-      //             patients.forEach(x => rooms.push(x.id));
-      //             resolve({ rooms: rooms, entity: user.id });
-      //           }, error => reject(error));
-      //         else resolve({ rooms: rooms, entity: user.id });
-      //       }, error => reject(error));
-      //     else reject(new Error("user not found"));
-      //   }, error => reject(error));
-      // // if is a vitabox, just return their own exchange
-      // else resolve({ rooms: [payload.id], entity: payload.id });
-
-
     });
   });
 }
